ec2109a4dfbf45d81971d165fea76141f669fe22
박찬희 <shine7065@naver.com>
2023-04-20 17:13:57 +0900

    [Modify] chatController 어노테이션 수정

--- server/src/main/java/com/talkeasy/server/config/WebSocketConfig.java
+++ server/src/main/java/com/talkeasy/server/config/WebSocketConfig.java
@@ -2,9 +2,11 @@ package com.talkeasy.server.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -17,6 +19,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("api/chat").withSockJS();
+        registry.addEndpoint("/chat").setAllowedOrigins("*").withSockJS();
     }
+
 }
