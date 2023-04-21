2ec24c6e1e88cd80045e93a098f0ed838889529b
박찬희 <shine7065@naver.com>
2023-04-21 09:50:05 +0900

    [Add] 카프카 리스너 구현 코드 추가

--- server/src/main/java/com/talkeasy/server/controller/chat/KafkaController.java
+++ server/src/main/java/com/talkeasy/server/controller/chat/KafkaController.java
@@ -1,6 +1,7 @@
 package com.talkeasy.server.controller.chat;
 
 import com.talkeasy.server.dto.MessageDto;
+import com.talkeasy.server.service.KafkaConsumerService;
 import com.talkeasy.server.service.chat.KafkaProducer;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.admin.NewTopic;
@@ -15,28 +16,42 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(value = "/kafka")
 public class KafkaController {
 
-    private final KafkaProducer producer;
+//    private final KafkaProducer producer;
+//
+//    @PostMapping
+//    public String sendMessage(@RequestBody MessageDto message) {
+//        this.producer.sendMessage(message);
+//        return "success";
+//    }
+//
+//    private final KafkaAdmin kafkaAdmin;
+//    private final KafkaTemplate<String, MessageDto> kafkaTemplate;
+//
+//    @PostMapping("/topic/create")
+//    public ResponseEntity<String> createTopic(@RequestBody String chatRoomId) {
+//        NewTopic newTopic = new NewTopic(chatRoomId, 1, (short) 1);
+//        kafkaAdmin.createOrModifyTopics(newTopic);
+//        return ResponseEntity.ok("Topic created successfully");
+//    }
+//
+//    @PostMapping("/chat/{chatRoomId}")
+//    @MessageMapping("/chat/kafka")
+//    public ResponseEntity<String> sendMessage(@PathVariable String chatRoomId, @RequestBody MessageDto message) {
+//        kafkaTemplate.send(chatRoomId, message);
+//        return ResponseEntity.ok("Message sent successfully");
+//    }
 
-    @PostMapping
-    public String sendMessage(@RequestBody MessageDto message) {
-        this.producer.sendMessage(message);
-        return "success";
+    private final KafkaConsumerService kafkaConsumerService;
+
+    @PostMapping("/subscribe/{topic}")
+    public ResponseEntity<String> subscribe(@PathVariable String topic) {
+        kafkaConsumerService.subscribe(topic);
+        return ResponseEntity.ok("Subscribed to topic: " + topic);
     }
 
-    private final KafkaAdmin kafkaAdmin;
-    private final KafkaTemplate<String, MessageDto> kafkaTemplate;
-
-    @PostMapping("/topic/create")
-    public ResponseEntity<String> createTopic(@RequestBody String chatRoomId) {
-        NewTopic newTopic = new NewTopic(chatRoomId, 1, (short) 1);
-        kafkaAdmin.createOrModifyTopics(newTopic);
-        return ResponseEntity.ok("Topic created successfully");
-    }
-
-    @PostMapping("/chat/{chatRoomId}")
-    @MessageMapping("/chat/kafka")
-    public ResponseEntity<String> sendMessage(@PathVariable String chatRoomId, @RequestBody MessageDto message) {
-        kafkaTemplate.send(chatRoomId, message);
-        return ResponseEntity.ok("Message sent successfully");
+    @PostMapping("/unsubscribe/{topic}")
+    public ResponseEntity<String> unsubscribe(@PathVariable String topic) {
+        kafkaConsumerService.unsubscribe(topic);
+        return ResponseEntity.ok("Unsubscribed from topic: " + topic);
     }
 }
