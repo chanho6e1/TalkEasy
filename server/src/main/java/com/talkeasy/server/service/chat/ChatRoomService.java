37401705157ab5aa5e693d06db9166afb6c1e04e
gms9424 <gms9424@naver.com>
2023-04-23 02:52:50 +0900

    [Fix] 깃 데이터 패칭 오류 테스트

--- server/src/main/java/com/talkeasy/server/service/chat/ChatRoomService.java
+++ server/src/main/java/com/talkeasy/server/service/chat/ChatRoomService.java
@@ -11,6 +11,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
 
+//사용 안함
+
 @Service
 public class ChatRoomService {
 
