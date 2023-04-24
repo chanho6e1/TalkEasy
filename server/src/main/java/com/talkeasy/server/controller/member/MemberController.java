7ebf653799de023f5dfc395c6be5fae93064580b
ai06010 <ai06010@naver.com>
2023-04-24 14:17:50 +0900

    [Add] s3 설정 완료

--- server/src/main/java/com/talkeasy/server/controller/member/MemberController.java
+++ server/src/main/java/com/talkeasy/server/controller/member/MemberController.java
@@ -2,6 +2,7 @@ package com.talkeasy.server.controller.member;
 
 
 import com.talkeasy.server.common.CommonResponse;
+import com.talkeasy.server.config.s3.S3Uploader;
 import com.talkeasy.server.domain.Member;
 import com.talkeasy.server.dto.user.MemberDetailResponse;
 import com.talkeasy.server.dto.user.MemberInfoUpdateRequest;
@@ -12,14 +13,12 @@ import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -32,6 +31,7 @@ import java.io.IOException;
 public class MemberController {
 
     private final MemberService memberService;
+    private final S3Uploader s3Uploader;
 
     @ApiOperation(value = "회원정보 수정하기", notes = "사진 변경, 이름 변경, 성별 변경, 생년월일 변경")
     @PostMapping(value = "/update", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
@@ -51,5 +51,15 @@ public class MemberController {
 
         return ResponseEntity.ok().body(CommonResponse.of("member 조회 성공", new MemberDetailResponse(oAuth2User.getMember())));
     }
+
+    @PostMapping("/image")
+    public ResponseEntity<CommonResponse> updateUserImage(@RequestParam("images") MultipartFile multipartFile) {
+        try {
+            log.info("============file: " + multipartFile);
+            s3Uploader.uploadFiles(multipartFile, "talkeasy");
+        } catch (Exception e) { return new ResponseEntity(HttpStatus.BAD_REQUEST); }
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+
 
 }
