ad428f88e4ca8759abbdc09609d4a23311857eb9
박찬희 <shine7065@naver.com>
2023-04-25 10:43:50 +0900

    [Modify] fcm classpath 파일명 수정

--- server/src/main/java/com/talkeasy/server/service/firebase/FirebaseCloudMessageService.java
+++ server/src/main/java/com/talkeasy/server/service/firebase/FirebaseCloudMessageService.java
@@ -62,7 +62,7 @@ public class FirebaseCloudMessageService {
 
     //firebase 서비스 인증 정보
     private String getAccessToken() throws IOException {
-        String firebaseConfigPath = "firebase/firebase_service_key.json";
+        String firebaseConfigPath = "firebase/talkeasy-1e037-firebase-adminsdk-cak0e-48150f03c5.json";
 
         GoogleCredentials googleCredentials = GoogleCredentials
                 .fromStream(new ClassPathResource(firebaseConfigPath).getInputStream())
