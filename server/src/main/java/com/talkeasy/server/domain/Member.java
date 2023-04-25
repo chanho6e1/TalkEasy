d906d3a1dc430b539a40dd44c7ec0bbbce564b3b
임지원 <wldnjs4255@naver.com>
2023-04-25 15:10:26 +0900

    [Modify] Member entity 소문자로 변경

--- server/src/main/java/com/talkeasy/server/domain/Member.java
+++ server/src/main/java/com/talkeasy/server/domain/Member.java
@@ -5,37 +5,18 @@ import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.persistence.Column;
-import javax.validation.constraints.Size;
-
 @Getter
-@Document(collection = "MEMBER")
+@Document(collection = "member")
 @Builder
 public class Member {
     @Id
     private String id;
-
-    @Column(name = "name", length = 100)
-    @Size(max = 100)
     private String name;
-
-    @Column(name = "email", length = 512, unique = false)
-    @Size(max = 512)
     private String email;
-    @Column(name = "imageUrl", length = 512)
-    @Size(max = 512)
     private String imageUrl;
-    @Column(name = "role")
     private Integer role; //0:보호자/1:피보호자
-    @Column(name = "gender")
     private Integer gender; //0:남/1:여
-    @Column(name = "age")
     private Integer age;
-
-//    @Column(name = "accessToken")
-//    private String accessToken;
-
-    @Column(name = "birthDate")
     private String birthDate;
 
 }
