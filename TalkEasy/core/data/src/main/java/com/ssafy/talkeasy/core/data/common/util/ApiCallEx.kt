0a864d6809d330ce89a29819846e478b37ef558a
이아름 <souk0712@naver.com>
2023-05-04 17:11:14 +0900

    [Modify] ApiCallEx.kt 매개변수 변경

--- TalkEasy/core/data/src/main/java/com/ssafy/talkeasy/core/data/common/util/ApiCallEx.kt
+++ TalkEasy/core/data/src/main/java/com/ssafy/talkeasy/core/data/common/util/ApiCallEx.kt
@@ -4,21 +4,13 @@ import com.ssafy.talkeasy.core.domain.Resource
 import java.io.IOException
 import kotlinx.coroutines.CoroutineDispatcher
 import retrofit2.HttpException
-import retrofit2.Response
 
 suspend fun <T> wrapToResource(
     dispatcher: CoroutineDispatcher,
     apiCall: suspend () -> T,
 ): Resource<T> {
     return try {
-        val response = apiCall()
-
-        if (response is Response<*>) {
-            val code = response.code()
-            Resource.Success(code, response)
-        }
-
-        Resource.Success(0, response)
+        Resource.Success(apiCall())
     } catch (throwable: Throwable) {
         when (throwable) {
             is IOException -> Resource.Error(throwable.message ?: "ERROR1")
