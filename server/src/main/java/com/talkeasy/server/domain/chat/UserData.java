289bd1b6e8ee168b303607bcc09ba33efec4fcd0
박찬희 <shine7065@naver.com>
2023-05-04 15:27:49 +0900

    [Fix] 채팅방 초기 생성 시, null exception 발생 문제 해결

--- server/src/main/java/com/talkeasy/server/domain/chat/UserData.java
+++ server/src/main/java/com/talkeasy/server/domain/chat/UserData.java
@@ -1,12 +1,10 @@
 package com.talkeasy.server.domain.chat;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 public class UserData {
     private Boolean nowIn; // 현재 채팅방에 들어와있는지 여부
     private String leaveTime; // 채팅방 떠난 시간
