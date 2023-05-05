ff65add615eab62801c18261def8ad4572689530
이아름 <souk0712@naver.com>
2023-05-05 21:58:29 +0900

    [Add] 프로필 편집 관련 dialog와 launcher 추가

--- TalkEasy/feature/common/src/main/java/com/ssafy/talkeasy/feature/common/component/EditProfile.kt
+++ TalkEasy/feature/common/src/main/java/com/ssafy/talkeasy/feature/common/component/EditProfile.kt
@@ -1,11 +1,23 @@
 package com.ssafy.talkeasy.feature.common.component
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.launch
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -14,6 +26,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -24,6 +37,8 @@ import com.ssafy.talkeasy.feature.common.R
 import com.ssafy.talkeasy.feature.common.ui.theme.dimens
 import com.ssafy.talkeasy.feature.common.ui.theme.md_theme_light_background
 import com.ssafy.talkeasy.feature.common.ui.theme.typography
+import com.ssafy.talkeasy.feature.common.util.toast
+import java.io.InputStream
 
 @OptIn(ExperimentalGlideComposeApi::class)
 @Composable
@@ -64,5 +79,90 @@ inline fun Modifier.noRippleClickable(crossinline onClick: () -> Unit): Modifier
         interactionSource = remember { MutableInteractionSource() }
     ) {
         onClick()
+    }
+}
+
+@Composable
+fun ShowProfileDialog(
+    modifier: Modifier = Modifier,
+    visible: Boolean,
+    onDismissRequest: () -> Unit = {},
+    onImagePicked: (Bitmap) -> Unit,
+) {
+    val context = LocalContext.current
+    val takePhotoFromCameraLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.TakePicturePreview()
+    ) { bitmap ->
+        if (bitmap != null) {
+            onImagePicked(bitmap)
+        } else {
+            context.toast(context.getString(R.string.content_camera_failed_message))
+        }
+    }
+    val takePhotoFromGalleryLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.GetContent()
+    ) { uri ->
+        if (uri != null) {
+            val inputStream: InputStream? = context.contentResolver.openInputStream(uri)
+            inputStream.use {
+                val bitmap = BitmapFactory.decodeStream(it)
+                onImagePicked(bitmap)
+            }
+        } else {
+            context.toast(context.getString(R.string.content_camera_failed_message))
+        }
+    }
+    if (visible) {
+        CustomAlertDialog(
+            onDismissRequest = { onDismissRequest() },
+            content = {
+                Surface(
+                    modifier = modifier
+                        .clip(
+                            RoundedCornerShape(10.dp)
+                        )
+                        .fillMaxWidth(),
+                    color = md_theme_light_background
+                ) {
+                    LazyColumn(
+                        modifier = Modifier,
+                        contentPadding = PaddingValues(18.dp),
+                        verticalArrangement = Arrangement.spacedBy(30.dp)
+                    ) {
+                        item {
+                            Text(
+                                text = stringResource(id = R.string.title_photo_picker),
+                                style = typography.titleMedium,
+                                fontWeight = FontWeight.Bold
+                            )
+                        }
+                        item {
+                            Text(
+                                text = stringResource(id = R.string.content_camera_select),
+                                style = typography.titleSmall,
+                                modifier = Modifier
+                                    .wrapContentHeight()
+                                    .fillMaxWidth()
+                                    .clickable {
+                                        takePhotoFromCameraLauncher.launch()
+                                    }
+                            )
+                        }
+                        item {
+                            Text(
+                                text = stringResource(id = R.string.content_gallery_select),
+                                style = typography.titleSmall,
+                                modifier = Modifier
+                                    .wrapContentHeight()
+                                    .fillMaxWidth()
+                                    .clickable {
+                                        takePhotoFromGalleryLauncher.launch("image/*")
+                                    }
+                            )
+                        }
+                    }
+                }
+            }
+        )
     }
 }
