27dfc3a0d2d8d5e69706704dee8506bf2026d050
gms9424 <gms9424@naver.com>
2023-05-08 23:36:59 +0900

    [Add] 팔로우관련 서비스 테스트 코드 추가(팔로우, 언팔로우, 팔로우 목록)

--- server/src/main/java/com/talkeasy/server/domain/member/Follow.java
+++ server/src/main/java/com/talkeasy/server/domain/member/Follow.java
@@ -2,6 +2,7 @@ package com.talkeasy.server.domain.member;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
