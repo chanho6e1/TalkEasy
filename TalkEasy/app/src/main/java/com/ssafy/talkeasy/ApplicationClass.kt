81419c3d472257ac817bf5e3021f80c6422cfd9a
dobbi030 <dmstjs7047@hanmail.net>
2023-05-08 14:20:46 +0900

    [Add] ApplicationClass 정의

---
+++ TalkEasy/app/src/main/java/com/ssafy/talkeasy/ApplicationClass.kt
@@ -0,0 +1,12 @@
+package com.ssafy.talkeasy
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ApplicationClass : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
