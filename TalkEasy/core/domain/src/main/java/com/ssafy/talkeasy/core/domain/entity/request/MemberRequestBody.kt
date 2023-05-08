f2a6ecc8947661bb81d905dd1f2b8683864eeea9
이아름 <souk0712@naver.com>
2023-05-08 21:02:48 +0900

    [Modify] age 제거

--- TalkEasy/core/domain/src/main/java/com/ssafy/talkeasy/core/domain/entity/request/MemberRequestBody.kt
+++ TalkEasy/core/domain/src/main/java/com/ssafy/talkeasy/core/domain/entity/request/MemberRequestBody.kt
@@ -4,7 +4,6 @@ data class MemberRequestBody(
     val accessToken: String,
     val role: Int,
     val name: String,
-    val age: Int = 0,
     val birthDate: String = "",
     val gender: Int = 0,
 )
