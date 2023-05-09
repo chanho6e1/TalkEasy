693dc012355c24d8e931b63d34ee7bee0395d612
ai06010 <ai06010@naver.com>
2023-05-09 17:33:11 +0900

    [Modify] db설정 env로하도록

--- flask-server/app.py
+++ flask-server/app.py
@@ -1,11 +1,12 @@
 import psycopg2
+import os
 
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
 app = Flask(__name__)
-app.config.from_envvar('APP_CONFIG_FILE')
+# app.config.from_envvar('APP_CONFIG_FILE')
 
 @app.route('/')
 def hello():
@@ -14,7 +15,12 @@ def hello():
 
 @app.route('/location', methods=['GET'])
 def get_location():
-    db.init_app(app)
+    # db.init_app(app)
+    db = psycopg2.connect(dbname=os.getenv('DB_NAME'),
+                          user=os.getenv('DB_USER'),
+                          host=os.getenv('DB_HOST'),
+                          password=os.getenv('DB_PASSWORD'),
+                          port=5432)
     cur = db.cursor()
     email = request.args.get('email')
     print(type(email))
