d1cddca499610cba046fc41ca683867e452e30fd
이아름 <souk0712@naver.com>
2023-05-10 23:37:31 +0900

    [Add] 데이터 값 추가

--- TalkEasy/core/domain/src/main/java/com/ssafy/talkeasy/core/domain/entity/response/Follow.kt
+++ TalkEasy/core/domain/src/main/java/com/ssafy/talkeasy/core/domain/entity/response/Follow.kt
@@ -3,9 +3,11 @@ package com.ssafy.talkeasy.core.domain.entity.response
 data class Follow(
     val userId: String,
     val userName: String,
+    val followId: String,
     val imageUrl: String,
     val memo: String,
     val mainStatus: Boolean,
+    val gender: Int?,
     val age: Int?,
     val birthDate: String?,
     val locationStatus: Boolean,
