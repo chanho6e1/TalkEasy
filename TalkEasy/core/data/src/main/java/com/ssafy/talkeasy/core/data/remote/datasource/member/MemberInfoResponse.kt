516b5953407ea91ed528a6a03f1c2a79347b814f
이아름 <souk0712@naver.com>
2023-05-10 17:08:50 +0900

    [Modify] MemberResponse 생성 및 사용

---
+++ TalkEasy/core/data/src/main/java/com/ssafy/talkeasy/core/data/remote/datasource/member/MemberInfoResponse.kt
@@ -0,0 +1,37 @@
+package com.ssafy.talkeasy.core.data.remote.datasource.member
+
+import com.google.gson.annotations.SerializedName
+import com.ssafy.talkeasy.core.data.remote.datasource.base.DataToDomainMapper
+import com.ssafy.talkeasy.core.domain.entity.response.MemberInfo
+
+data class MemberInfoResponse(
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("userName")
+    val userName: String,
+    @SerializedName("email")
+    val email: String,
+    @SerializedName("imageUrl")
+    val imageUrl: String,
+    @SerializedName("role")
+    val role: Int,
+    @SerializedName("age")
+    val age: Int?,
+    @SerializedName("gender")
+    val gender: Int?,
+    @SerializedName("birthDate")
+    val birthDate: String?,
+) : DataToDomainMapper<MemberInfo> {
+
+    override fun toDomainModel(): MemberInfo =
+        MemberInfo(
+            userId = id,
+            userName = userName,
+            email = email,
+            imageUrl = imageUrl,
+            role = role,
+            age = age,
+            gender = gender,
+            birthDate = birthDate
+        )
+}
