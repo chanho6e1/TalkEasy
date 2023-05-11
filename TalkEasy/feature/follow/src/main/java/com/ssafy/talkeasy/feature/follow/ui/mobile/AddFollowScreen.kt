8ab16ef535f7b09d82823d7909ed123b57b6631e
이아름 <souk0712@naver.com>
2023-05-12 03:05:05 +0900

    [Modify] CustomAlertDialog 삭제 및 사용 부분 Dialog로 대체

--- TalkEasy/feature/follow/src/main/java/com/ssafy/talkeasy/feature/follow/ui/mobile/AddFollowScreen.kt
+++ TalkEasy/feature/follow/src/main/java/com/ssafy/talkeasy/feature/follow/ui/mobile/AddFollowScreen.kt
@@ -35,7 +35,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.ssafy.talkeasy.feature.common.component.CustomAlertDialog
+import androidx.compose.ui.window.Dialog
 import com.ssafy.talkeasy.feature.common.component.CustomTextField
 import com.ssafy.talkeasy.feature.common.component.NoLabelTextField
 import com.ssafy.talkeasy.feature.common.component.Profile
@@ -152,7 +152,7 @@ fun AddNotificationDialog(
     }
 
     if (visible) {
-        CustomAlertDialog(
+        Dialog(
             onDismissRequest = { onDismissRequest() }
         ) {
             Surface(
