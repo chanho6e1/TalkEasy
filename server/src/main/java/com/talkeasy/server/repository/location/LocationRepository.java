24bd97d0bd020fd6ea396798306ca747b7200fcd
임지원 <wldnjs4255@naver.com>
2023-05-12 14:20:12 +0900

    [Feat] 당일 위치 분석 api

--- server/src/main/java/com/talkeasy/server/repository/location/LocationRepository.java
+++ server/src/main/java/com/talkeasy/server/repository/location/LocationRepository.java
@@ -4,8 +4,14 @@ import com.talkeasy.server.domain.location.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface LocationRepository extends JpaRepository<Location, Long> {
 
     Location findTopByUserIdOrderByDateTimeDesc(String userId);
+
+    List<Location> findByUserIdAndDateTimeAfter(String userId, LocalDateTime startTimeOfToday);
+
 }
