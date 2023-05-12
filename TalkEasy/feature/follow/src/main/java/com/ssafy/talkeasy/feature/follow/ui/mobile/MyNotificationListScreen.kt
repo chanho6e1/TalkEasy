5cd67d4acecd29e6daa8bf31f3d4dc2e600c1fed
이아름 <souk0712@naver.com>
2023-05-12 10:46:50 +0900

    [Add] MyNotificationItem 분리 및 type에 따른 처리

--- TalkEasy/feature/follow/src/main/java/com/ssafy/talkeasy/feature/follow/ui/mobile/MyNotificationListScreen.kt
+++ TalkEasy/feature/follow/src/main/java/com/ssafy/talkeasy/feature/follow/ui/mobile/MyNotificationListScreen.kt
@@ -1,45 +1,27 @@
 package com.ssafy.talkeasy.feature.follow.ui.mobile
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ssafy.talkeasy.core.domain.entity.response.MyNotificationItem
+import com.ssafy.talkeasy.feature.common.component.MyNotificationListItem
 import com.ssafy.talkeasy.feature.common.component.NoContentLogoMessage
 import com.ssafy.talkeasy.feature.common.component.ProtectorHeader
-import com.ssafy.talkeasy.feature.common.ui.theme.black_squeeze
-import com.ssafy.talkeasy.feature.common.ui.theme.delta
 import com.ssafy.talkeasy.feature.common.ui.theme.typography
-import com.ssafy.talkeasy.feature.common.util.toTimeString
 import com.ssafy.talkeasy.feature.follow.R
 
 @Composable
-fun MyNotificationListRoute(
-    modifier: Modifier = Modifier,
-) {
+fun MyNotificationListRoute(modifier: Modifier = Modifier) {
     val myNotificationList = remember {
         arrayListOf<MyNotificationItem>()
     }
@@ -81,65 +63,6 @@ fun MyNotificationListContent(
             width = 156,
             height = 72,
             betweenValue = 20
-        )
-    }
-}
-
-@Composable
-fun MyNotificationListItem(
-    modifier: Modifier = Modifier,
-    item: MyNotificationItem,
-    onItemClicked: () -> Unit = {},
-) {
-    Column(
-        modifier = modifier
-            .fillMaxWidth()
-            .wrapContentHeight()
-            .clickable { onItemClicked() }
-    ) {
-        Row(
-            modifier = modifier
-                .fillMaxWidth()
-                .wrapContentHeight(),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Image(
-                modifier = modifier.size(60.dp),
-                painter = painterResource(id = R.drawable.ic_siren_circle),
-                contentDescription = stringResource(id = R.string.image_siren_circle)
-            )
-            Column(
-                modifier = Modifier
-                    .padding(
-                        start = 15.dp,
-                        end = 18.dp,
-                        top = 18.dp,
-                        bottom = 18.dp
-                    )
-                    .fillMaxWidth()
-                    .wrapContentHeight(),
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                Text(
-                    modifier = Modifier,
-                    text = item.message,
-                    style = typography.bodyMedium,
-                    fontWeight = FontWeight.Bold
-                )
-                Text(
-                    modifier = modifier,
-                    color = delta,
-                    text = item.time.toTimeString(),
-                    style = typography.bodyMedium,
-                    textAlign = TextAlign.Center
-                )
-            }
-        }
-        Box(
-            modifier = Modifier
-                .height(1.dp)
-                .fillMaxWidth()
-                .background(black_squeeze)
         )
     }
 }
