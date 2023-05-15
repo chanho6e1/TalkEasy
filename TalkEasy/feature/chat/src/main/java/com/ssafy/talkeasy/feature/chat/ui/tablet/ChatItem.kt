881bcea1471a90fa07483dec82104a5f9dfbd5c0
dobbi030 <dmstjs7047@hanmail.net>
2023-05-15 13:12:47 +0900

    [Fix] 이중 스크롤로 인한 오류 수정

--- TalkEasy/feature/chat/src/main/java/com/ssafy/talkeasy/feature/chat/ui/tablet/ChatItem.kt
+++ TalkEasy/feature/chat/src/main/java/com/ssafy/talkeasy/feature/chat/ui/tablet/ChatItem.kt
@@ -1,9 +1,10 @@
-package com.ssafy.talkeasy.feature.chat.ui.tablet.balloon
+package com.ssafy.talkeasy.feature.chat.ui.tablet
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,11 +22,11 @@ fun PartnerChat(chatPartner: Follow, messages: List<Chat>) {
         Box {
             PartnerChatItemHead(chatPartner = chatPartner, type = messages[0].type)
 
-            LazyColumn(
+            Column(
                 modifier = Modifier.padding(start = 47.dp, top = 19.dp),
                 verticalArrangement = Arrangement.spacedBy(10.dp)
             ) {
-                items(messages.size) { index ->
+                repeat(messages.size) { index ->
                     ChatBalloon(
                         chatDirection = ChatDirection.PARTNER,
                         chat = messages[index],
@@ -40,15 +41,15 @@ fun PartnerChat(chatPartner: Follow, messages: List<Chat>) {
 @Composable
 fun MyChat(messages: List<Chat>) {
     if (messages.isNotEmpty()) {
-        Box(contentAlignment = Alignment.TopEnd) {
+        Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.TopEnd) {
             MyChatItemHead(messages[0].type)
 
-            LazyColumn(
+            Column(
                 modifier = Modifier.padding(end = 10.dp),
                 verticalArrangement = Arrangement.spacedBy(10.dp),
                 horizontalAlignment = Alignment.End
             ) {
-                items(messages.size) { index ->
+                repeat(messages.size) { index ->
                     ChatBalloon(
                         chatDirection = ChatDirection.ME,
                         chat = messages[index],
