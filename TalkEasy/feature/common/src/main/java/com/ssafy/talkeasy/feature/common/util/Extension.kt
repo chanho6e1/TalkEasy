040dfbcff336b185d7bf2dc0bebc0e2bdf22aa1e
이아름 <souk0712@naver.com>
2023-05-17 05:23:50 +0900

    [Add] 현재 시간을 반환하는 함수 구현

--- TalkEasy/feature/common/src/main/java/com/ssafy/talkeasy/feature/common/util/Extension.kt
+++ TalkEasy/feature/common/src/main/java/com/ssafy/talkeasy/feature/common/util/Extension.kt
@@ -2,8 +2,10 @@ package com.ssafy.talkeasy.feature.common.util
 
 import android.content.Context
 import android.widget.Toast
+import java.text.SimpleDateFormat
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.Date
 import java.util.Locale
 
 fun Context.toast(message: String) {
@@ -13,4 +15,10 @@ fun Context.toast(message: String) {
 fun String.toTimeString(): String {
     val dateTime = LocalDateTime.parse(this, DateTimeFormatter.ISO_LOCAL_DATE_TIME)
     return dateTime.format(DateTimeFormatter.ofPattern("a HH:mm", Locale.KOREA))
+}
+
+fun getCurrentDateTime(): String {
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSSSS", Locale.getDefault())
+    val currentTime = Date(System.currentTimeMillis())
+    return dateFormat.format(currentTime)
 }
