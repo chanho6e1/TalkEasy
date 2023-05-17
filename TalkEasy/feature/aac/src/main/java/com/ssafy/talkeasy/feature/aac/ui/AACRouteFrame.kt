792a4f500128baa58bdf4a7925d197a27dc8a662
dobbi030 <dmstjs7047@hanmail.net>
2023-05-17 14:35:01 +0900

    [Add] aac 단어 선택 시 나타나는 연관 단어 layout 구현

--- TalkEasy/feature/aac/src/main/java/com/ssafy/talkeasy/feature/aac/ui/AACRouteFrame.kt
+++ TalkEasy/feature/aac/src/main/java/com/ssafy/talkeasy/feature/aac/ui/AACRouteFrame.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -295,10 +296,17 @@ fun ConstraintLayoutScope.AACBox(
         } else {
             AACFixedCards()
 
-            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
                 if (category == "") {
                     AACCategory(isOpened = isOpened)
                 } else {
+                    Spacer(modifier = Modifier.padding(top = 8.dp))
+
+                    AACRelatedCards()
+
                     AACCardBox(category = category)
                 }
             }
@@ -313,14 +321,14 @@ fun AACCardBox(category: String, aacViewModel: AACViewModel = viewModel()) {
         mutableStateOf(0)
     }
     val aacWordListSize = aacWordList?.aacList?.size ?: 0
-    val wordCountPerPage = 20
+    val wordCountPerPage = 16
     val totalPageCount =
         aacWordListSize / wordCountPerPage + if (aacWordListSize % wordCountPerPage == 0) 0 else 1
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(top = 20.dp),
+            .padding(top = 14.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
--- TalkEasy/feature/aac/src/main/java/com/ssafy/talkeasy/feature/aac/ui/AACWord.kt
+++ TalkEasy/feature/aac/src/main/java/com/ssafy/talkeasy/feature/aac/ui/AACWord.kt
@@ -1,7 +1,11 @@
 package com.ssafy.talkeasy.feature.aac.ui
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -9,6 +13,8 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
@@ -21,7 +27,13 @@ import com.ssafy.talkeasy.core.domain.entity.response.AACWord
 import com.ssafy.talkeasy.feature.aac.AACViewModel
 import com.ssafy.talkeasy.feature.aac.R
 import com.ssafy.talkeasy.feature.common.R.drawable
+import com.ssafy.talkeasy.feature.common.ui.theme.md_theme_light_background
+import com.ssafy.talkeasy.feature.common.ui.theme.md_theme_light_primary
+import com.ssafy.talkeasy.feature.common.ui.theme.md_theme_light_secondary
+import com.ssafy.talkeasy.feature.common.ui.theme.md_theme_light_secondaryContainer
 import com.ssafy.talkeasy.feature.common.ui.theme.seed
+import com.ssafy.talkeasy.feature.common.ui.theme.shapes
+import com.ssafy.talkeasy.feature.common.ui.theme.typography
 
 @Composable
 fun AACFixedCards(aacViewModel: AACViewModel = viewModel()) {
@@ -46,11 +58,60 @@ fun AACFixedCards(aacViewModel: AACViewModel = viewModel()) {
 }
 
 @Composable
+fun AACRelatedCards(aacViewModel: AACViewModel = viewModel()) {
+    val relatedWordList = listOf<AACWord>()
+
+    Box(modifier = Modifier.padding(start = 30.dp)) {
+        Surface(
+            modifier = Modifier.padding(top = 6.dp),
+            border = BorderStroke(width = 1.dp, color = seed),
+            shape = shapes.extraSmall
+        ) {
+            LazyRow(
+                modifier = Modifier
+                    .padding(top = 20.dp, bottom = 10.dp, start = 10.dp, end = 8.dp)
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                if (relatedWordList.isEmpty()) {
+                    item {
+                        Text(
+                            modifier = Modifier.padding(horizontal = 14.dp, vertical = 12.dp),
+                            text = "연관 단어가 없습니다",
+                            color = md_theme_light_secondary,
+                            style = typography.titleMedium
+                        )
+                    }
+                } else {
+                    items(items = relatedWordList) { word ->
+                        AACCardWrap(
+                            word = word.title,
+                            color = md_theme_light_secondaryContainer,
+                            onCardSelectedListener = { aacViewModel.addCard(word.title) }
+                        )
+                    }
+                }
+            }
+        }
+
+        Text(
+            modifier = Modifier
+                .padding(start = 7.dp)
+                .background(color = md_theme_light_background)
+                .padding(horizontal = 3.dp),
+            text = "연관 단어 목록",
+            color = md_theme_light_primary,
+            style = typography.bodyMedium
+        )
+    }
+}
+
+@Composable
 fun AACSmallCards(page: Int = 0, wordCountPerPage: Int, wordList: List<AACWord>) {
     LazyVerticalGrid(
         columns = GridCells.Fixed(4),
         horizontalArrangement = Arrangement.spacedBy(15.dp),
-        verticalArrangement = Arrangement.spacedBy(15.dp),
+        verticalArrangement = Arrangement.spacedBy(13.dp),
         reverseLayout = false,
         userScrollEnabled = false
     ) {
