1debff55730ac9d911c56d28753a7300f3201d25
gms9424 <gms9424@naver.com>
2023-05-19 02:03:15 +0900

    [Modify] alarm response 변경

--- server/src/main/java/com/talkeasy/server/dto/alarm/ResponseWardAlarmDto.java
+++ server/src/main/java/com/talkeasy/server/dto/alarm/ResponseWardAlarmDto.java
@@ -13,14 +13,17 @@ import lombok.Setter;
 public class ResponseWardAlarmDto {
     private String alarmId;
     private String roomId;
+    private String chatId;
     private String content;
     private String created_dt;
     private Boolean readStatus; // 읽음 여부 true/false
     private String fromUserName; // 요청을 보낸 사람
 
+
     public ResponseWardAlarmDto(Alarm alarm) {
         this.alarmId = alarm.getId();
         this.roomId = alarm.getRoomId();
+        this.chatId = alarm.getChatId();
         this.content = alarm.getContent();
         this.readStatus = alarm.getReadStatus();
         this.fromUserName = alarm.getFromName();
